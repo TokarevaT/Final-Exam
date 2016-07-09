076546e0fc200db43724857364bdc11ff831a31c
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-07-09 17:46:24 +0300

    added ajax,carousel-slider

--- Final Exam/js/ajax.js
+++ Final Exam/js/ajax.js
@@ -1,103 +1,6 @@
-﻿'use strict';
-
-var human = {
-    name: 'Jack',
-    age: 25,
-    gender: 'male',
-    height: 165,
-    weight: 50,
-};
-console.log(human);
-
-var worker = {
-    work: function () {
-        place: 'DTC';
-        salary: 1000;
-    }
-};
-
-worker.work();
-console.log(worker.work);
-
-var student = {
-    watchTV: function () {
-        learning: 'National University';
-        grant: 2500;
-    }
-};
-
-student.watchTV();
-console.log(student);
-
-worker.__proto__ = human;
-console.log('-------------');
-console.log('worker.name =', worker.name);
-console.log('worker.age =', worker.age);
-console.log('worker.gender =', worker.gender);
-console.log('worker.height =', worker.height);
-
-student.__proto__ = human;
-console.log('-------------');
-console.log('student.name =', student.name);
-console.log('student.age =', student.age);
-console.log('student.gender =', student.gender);
-console.log('student.height =', student.height);
-
-function Human() {
-    this.name = 'Jack',
-    this.age = 25,
-    this.gender = 'male',
-    this.height = 165,
-    this.weight = 50
-}
-var newHuman = new Human();
-console.log('newHuman =', newHuman);
-
-function Work() {
-    this.place = 'DTC';
-    this.salary = 1000;
-}
-
-Work.prototype = newHuman;
-
-var newWork = new Work();
-console.log('newWork =', newWork);
-
-console.log('newWork.weight =', newWork.weight);
-console.log('newWork.age =', newWork.age);
-console.log('newWork.name =', newWork.name);
-
-function WatchTV() {
-    this.learning = 'National University';
-    this.grant = 2500;
-}
-WatchTV.prototype = newHuman;
-
-var newWatchTV = new WatchTV();
-console.log('newWatchTV =', newWatchTV);
-
-
-console.log('newWatchTV.name =', newWatchTV.name);
-console.log('newWatchTV.gender =', newWatchTV.gender);
-console.log('newWatchTV.height =', newWatchTV.height);
-
-$(function () {
-
-    $('button').click(function (e) {
-        $.ajax({
-            url:
-            'https://pixabay.com/api/?key=2750116-f516471249e70495e72331045&q=yellow+flowers&image_type=photo' + 'test' + 'callback=GoogleCallback&context=?',
-            dataType: 'jsonp'
-
-        })
-    });
-
-
-});
-
-
+﻿
 'use strict'; // Проверяем на валидность
-$(function () {
+
     var ajaxModule = function () { };
     ajaxModule.prototype = {
         iterator: 1,
@@ -136,15 +39,25 @@ $(function () {
         // ----------------------------------------------------------- Вывод результатов
         parseResponse: function (response) {
             var self = this;
-
-            $.each(response.hits, function (index, value) {
-                $(".result").prepend("<div class='image image" + index + "' style='width:" + value.webformatWidth + "px; height:" + value.webformatHeight + "px; background: url(" + value.webformatURL + ");'><a href='" + value.pageURL + "' target='_blank'><div class='overlay'></div></a><div class='hidden'></div></div>");
-                $(".image" + index + " .hidden").append("<div>User: <b>" + value.user + "</b></div><div>Tags: <b>" + value.tags + "</b></div><div class='stats'><i class='fa fa-eye'></i> <b>Views " + value.views + "</b> &nbsp; <i class='fa fa-thumbs-o-up'></i> <b> Likes " + value.likes + "</b></div><div class='direct-links'><a href='" + value.webformatURL + "' target='_blank'><i class='fa fa-link'></i>  Direct Link</a> <a href='" + value.webformatURL + "' download><i class='fa fa-download'></i> Download</a></div>");
-            });
-            if (response.hits == 0) {
-                $(".result").prepend("<div class='error'>Sorrow, no result :(</div>");
+            
+            if (response.hits != 0) {
+                var divs = $('.grid .grid-item');
+                for (var i = 0; i < divs.length; i++) {
+                    if (i <= response.hits.length) {
+                        var width = 300;
+                        var element = $(divs[i]);
+                        if (element.hasClass('grid-item--width2')) {
+                            width = 320;
+                        }
+                        else if (element.hasClass('grid-item--width3')) {
+                            width = 480;
+                        }
+                        $(divs[i]).empty();
+                        $(divs[i]).append("<div class='image image" + i + "' style='width:" + width + "px; height:310px;border-radius: 5px; background: url(" + response.hits[i].webformatURL + ");'><a href='" + response.hits[i].pageURL + "' target='_blank'><div class='overlay'></div></a><div class='hidden'></div></div>");
+                    }
+                }
             }
-
+      
             clearTimeout(self.masonryTimeoutClear);
         },
     }
@@ -169,4 +82,10 @@ $(function () {
             }
         }, 500);
     });
-});
+
+
+    function clickSearch() {
+        var textValue = $('._sw').val();
+        debugger;
+       // ajaxModule.callAjax(textValue, callback);
+    }
